41320341e560282ed1b56148628c256a89a312dd
Owen Wright <owen.wright@noutc.com>
2022-06-15 12:45:26 -0400

    Refactored Lowest Score Calculations

---
+++ ClassInformation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grade.Tool
+{
+    public class ClassInformation
+    {
+        public string ClassName { get; set; } = default!;
+        public double Q1Grade { get; set; }
+        public double Q2Grade { get; set; }
+    }
+}
